7a131f4f782a601d0ff5d61d6bf1c92378e183ff
Sergei <fedoseev_job@internet.ru>
2022-07-27 00:31:03 +0300

    Финальная версия

--- src/main/scala/com/testClass/AutoTest.scala
+++ src/main/scala/com/testClass/AutoTest.scala
@@ -234,6 +234,7 @@ case class AutoTest(private val spark: SparkSession) {
         false
       case ex: MatchError => // Данный вид ошибки получаем, если source файл содержит более двух столбцов
         appendTestRes(sourceDDLPath, "!!! Check source file. This file should contain only the attribute name and data type !!!")
+        false
       case ex: Throwable => { // Другие ошибки
         appendTestRes(sourceDDLPath, ex.getMessage)
         false
